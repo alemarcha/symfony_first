3bdbc6e4126a11b797de6e3da3c9f30e9e961798
Alexis Martínez <alemarcha26@gmail.com>
2017-03-26 15:48:25 +0200

    First API endpoint.

--- src/AppBundle/Controller/GenusParamTemplateController.php
+++ src/AppBundle/Controller/GenusParamTemplateController.php
@@ -9,8 +9,10 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusParamTemplateController extends Controller {
@@ -27,10 +29,32 @@ class GenusParamTemplateController extends Controller {
             'I counted 8 legs... as they wrapped around me',
             'Inked!'
         ];
+
         return $this->render("genus/show.html.twig", [
-            'name' => $paramsName,
+            'name'  => $paramsName,
             'notes' => $notes
         ]);
+
+    }
+
+    /**
+     * @Route("/genusTemplate/{paramsName}/notes")
+     * @Method("GET")
+     * @return Response
+     */
+    public function getNotesAction()
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            "notes" => $notes,
+        ];
+
+        return new JsonResponse($data);
 
     }
 
